68941cb20286c643f18a9c8ff38004bb1752cdf4
MaddyKm <134648462+MaddyKm@users.noreply.github.com>
2023-09-20 18:23:09 -0400

    gets surver functionality up and running with routes to different html pages

---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+//DEPENDENCIES
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+//DATA
+
+//APP/PORT
+const PORT = 3001;
+const app = express();
+const api = require("./routes/api");
+
+//MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+//ROUTES
+// app.use("api", api);
+
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+//START THE SERVER
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
